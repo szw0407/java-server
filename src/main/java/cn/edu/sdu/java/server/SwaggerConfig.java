506b67acac17bfa33c1ad22ae49a4105b7caa222
Vincent Son <107471539+szw0407@users.noreply.github.com>
2025-03-08 16:46:05 +0800

    Enable Swagger

---
+++ src/main/java/cn/edu/sdu/java/server/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package cn.edu.sdu.java.server;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI();
+    }
+}
