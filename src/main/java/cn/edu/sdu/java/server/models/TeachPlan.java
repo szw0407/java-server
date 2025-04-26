4b5a0e3ee322aa242982ab6198aab8ca7e719079
Vincent <107471539+szw0407@users.noreply.github.com>
2025-04-27 02:33:15 +0800

    teach plan

--- src/main/java/cn/edu/sdu/java/server/models/TeachPlan.java
+++ src/main/java/cn/edu/sdu/java/server/models/TeachPlan.java
@@ -24,6 +24,9 @@ public class TeachPlan {
     @JoinColumn(name = "course_id")
     private Course course;
 
+    // 课程计划的鉴别码，用于区分开设相同课程的不同教学班
+    private Integer teachPlanCode;
+
     @ManyToOne
     @JoinColumn(name = "person_id")
     private Teacher teacher;
@@ -31,7 +34,7 @@ public class TeachPlan {
     private Integer semester;
     private Integer year;
 
-    private Float grade;   // 老师被评价的得分
+    private Float grade;   // 老师被评价的得分，这个功能再说，没时间就不做了
 
     @OneToMany(mappedBy = "teachPlan", cascade = CascadeType.ALL, orphanRemoval = true)
         private List<Score> scores;
