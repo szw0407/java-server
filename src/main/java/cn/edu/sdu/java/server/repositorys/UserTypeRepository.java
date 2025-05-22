22905228e2be5226c4e5f26803b4f073dee270b5
Lee-666-ya <1256030532@qq.com>
2025-05-23 01:03:33 +0800

    fix usertype

--- src/main/java/cn/edu/sdu/java/server/repositorys/UserTypeRepository.java
+++ src/main/java/cn/edu/sdu/java/server/repositorys/UserTypeRepository.java
@@ -11,5 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserTypeRepository extends JpaRepository<UserType, Long> {
-    UserType findByName(EUserType name);
+    UserType findByName(String name);
 }
